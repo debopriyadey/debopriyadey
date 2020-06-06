7f2a1f95ac7cc442f17c3caa2a3de8dda9573f72
Debopriya Dey <64556550+creative-chaos@users.noreply.github.com>
2020-06-06 10:21:34 +0530

    Add files via upload

---
+++ js/portfoliojs.js
@@ -0,0 +1,31 @@
+ //user = prompt("what is youe name");
+ //alert("hello" + " " +user);
+ 
+ /*function navline() {
+  var x = document.getElementById("mynav");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}*/
+
+/*var navbar = document.getElementById("mynav");
+
+function openNav() {
+  navbar.classList.add("overlay");
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  navbar.classList.remove("overlay");
+  document.getElementById("myNav").style.height = "0%";
+}*/
+
+function openNav() {
+  document.getElementById("myNav1").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav1").style.height = "0%";
+}
